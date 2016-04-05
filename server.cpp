7b642406329f6905e8d3c0d4854975bb6617c3ae
LittleBird <ilms49898723@gmail.com>
2016-04-05 10:41:13 +0900

    add base functions init(), add accept() and fork() in main()

--- server.cpp
+++ server.cpp
@@ -19,14 +19,46 @@
 #include <cctype>
 
 bool isValidArguments(int argc, char const *argv[]);
+int birdRead(const int& fd, char* buffer, const int& n);
+int birdWrite(const int& fd, const char* buffer, const int& n);
+int serverInit(const int& port);
+void tcpServer(int fd);
+void sigChld(int signo);
 
 int main(int argc, char const *argv[])
 {
+    // local constant variables
+    constexpr static socklen_t clientLen = sizeof(sockaddr_in);
+    // check arguments
     if (argc != 2 || !isValidArguments(argc, argv)) {
         fprintf(stderr, "usage: %s PORT\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-
+    // server initialize
+    int port;
+    sscanf(argv[1], "%d", &port);
+    int listenId = serverInit(port);
+    // signal
+    signal(SIGCHLD, sigChld);
+    // wait for connection, fork()
+    while (true) {
+        pid_t childPid;
+        sockaddr_in clientAddr;
+        int clientfd = accept(listenId, reinterpret_cast<sockaddr*>(&clientAddr), &clientLen);
+        if ((childPid = fork()) == 0) {
+            close(listenId);
+            char clientInfo[1024];
+            strcpy(clientInfo, inet_ntoa(clientAddr.sin_addr));
+            int clientPort = static_cast<int>(clientAddr.sin_port);
+            fprintf(stdout, "Connection from %s port %d\n", clientInfo, clientPort);
+            tcpServer(clientfd);
+            exit(EXIT_SUCCESS);
+        }
+        else {
+            fprintf(stdout, "Start child Process pid = %d\n", static_cast<int>(pid));
+        }
+        close(clientfd);
+    }
     return 0;
 }
 
@@ -41,4 +73,47 @@ bool isValidArguments(int argc, char const *argv[]) {
         }
     }
     return true;
+}
+
+int birdRead(const int& fd, char* buffer, const int& n) {
+    int byteRead = read(fd, buffer, n);
+    if (byteRead < 0) {
+        fprintf(stderr, "Read Error\n");
+    }
+    return byteRead;
+}
+
+int birdWrite(const int& fd, const char* buffer, const int& n) {
+    int byteWrite = write(fd, buffer, sizeof(char) * n);
+    if (byteWrite < 0) {
+        fprintf(stderr, "Write Error\n");
+    }
+    return byteWrite;
+}
+
+int serverInit(const int& port) {
+    int listenId;
+    sockaddr_in serverAddr;
+    listenId = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddr.sin_port = htons(port);
+    if (bind(listenId, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(sockaddr_in)) < 0) {
+        fprintf(stderr, "Bind Error\n");
+        exit(EXIT_FAILURE);
+    }
+    listen(listenId, 64);
+}
+
+void tcpServer(int fd) {
+
+}
+
+void sigChld(int signo) {
+    pid_t pid;
+    int stat;
+    while ((pid = waitpid(-1, &stat, WCONTINUED)) != -1) {
+        fprintf(stdout, "Child process %d terminated.\n", static_cast<int>(pid));
+    }
 }
